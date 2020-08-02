e6e0c26bc1756da7f52dab9427d88dbd5f9377c3
Hoang Van Hau <phanhoangkag@gmail.com>
2020-08-02 22:40:35 +0700

    Updates

---
+++ precache-manifest.43caa58104f82696f0d3cd86fe76436e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0604ec0fab9f29462a2e064f92a159b",
+    "url": "/Redux-ToDoApp/index.html"
+  },
+  {
+    "revision": "a5b01697970a50a8f677",
+    "url": "/Redux-ToDoApp/static/css/main.feeaf00b.chunk.css"
+  },
+  {
+    "revision": "7638576c5ac724353723",
+    "url": "/Redux-ToDoApp/static/js/2.677b6ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Redux-ToDoApp/static/js/2.677b6ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b01697970a50a8f677",
+    "url": "/Redux-ToDoApp/static/js/main.743f9c19.chunk.js"
+  },
+  {
+    "revision": "8c6cffa5fc2635bc3856",
+    "url": "/Redux-ToDoApp/static/js/runtime-main.ae8f00a6.js"
+  }
+]);
